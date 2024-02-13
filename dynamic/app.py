e6fda42569d845439d993d4d84441122d6f79210
Jonathan <escobarj6@southernct.edu>
2024-02-12 19:12:25 -0500

    update dynamic app fruits

--- dynamic/app.py
+++ dynamic/app.py
@@ -1,6 +1,13 @@
+import random
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def greeting():
     return "Hello, there!"
+
+@app.route('/fruits')
+def fruits():
+    fruits = ['Apple', 'Banana', 'Orange', 'Pineapple', 'Strawberry']
+    index = random.randint(0, len(fruits))
+    return fruits[index]
